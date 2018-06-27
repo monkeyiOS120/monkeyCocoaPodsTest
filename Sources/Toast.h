52fe44dccb2e1e91513085c00291f919784e1c6b
monkey <wangyadong@hongtao-edu.com>
2018-06-27 18:03:54 +0800

    init

---
+++ Sources/Toast.h
@@ -0,0 +1,18 @@
+//
+//  Toast.h
+//  EffectiveObjcDemo
+//
+//  Created by monkey on 2018/6/27.
+//  Copyright © 2018年 monkey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Toast : NSObject
+
+/**
+ 两数相加
+ */
+-(NSUInteger)addWithA:(NSUInteger)a B:(NSUInteger) b;
+
+@end
---
+++ Sources/Toast.m
@@ -0,0 +1,18 @@
+//
+//  Toast.m
+//  EffectiveObjcDemo
+//
+//  Created by monkey on 2018/6/27.
+//  Copyright © 2018年 monkey. All rights reserved.
+//
+
+#import "Toast.h"
+
+@implementation Toast
+
+-(NSUInteger)addWithA:(NSUInteger)a B:(NSUInteger)b
+{
+    return a+b;
+}
+
+@end
